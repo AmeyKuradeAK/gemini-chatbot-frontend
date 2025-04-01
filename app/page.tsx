aa14e1a32e69d2307888eb59c5d227109d232c0a
Ammey Kuraaday <ameykurade60@gmail.com>
2025-04-01 20:39:06 +0530

    API endpoint changed

--- app/page.tsx
+++ app/page.tsx
@@ -13,7 +13,7 @@ export default function Chatbot() {
     setIsLoading(true); // Start loading animation
 
     try {
-      const res = await fetch("http://localhost:5000/chat", {
+      const res = await fetch("https://gemini-chatbot-backend-nesa.onrender.com", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ message }),
